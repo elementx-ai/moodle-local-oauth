d034e2973c33bec4f334481214d37d52bdf42d99
Daniel Jimenez <daniel.jimenez@spark64.com>
2021-11-26 12:19:11 +1300

    Add ability to retrive users tags

--- user_info.php
+++ user_info.php
@@ -19,7 +19,9 @@ if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
 $token = $server->getAccessTokenData(OAuth2\Request::createFromGlobals());
 if (isset($token['user_id']) && !empty($token['user_id'])) {
 
-    $user = $DB->get_record('user', array('id'=>$token['user_id']), 'id,auth,username,idnumber,firstname,lastname,email,lang,country,phone1,address,description');
+    $user = $DB->get_record_sql('SELECT id,auth,username,idnumber,firstname,lastname,email,lang,city,country,phone1,address,description FROM {user} WHERE id = :id', ['id' => $token['user_id']]);
+    $tags = $DB->get_record_sql('SELECT id,user_id,name,rawname FROM {tag} WHERE user_id = :id', ['id' => $token['user_id']]);
+
     if (!$user) {
         $logparams = array('other' => array('cause' => 'user_not_found'));
         $event = \local_oauth\event\user_info_request_failed::create($logparams);
@@ -43,6 +45,7 @@ if (isset($token['user_id']) && !empty($token['user_id'])) {
     $logparams = array('userid' => $user->id);
     $event = \local_oauth\event\user_info_request::create($logparams);
     $event->trigger();
+    $user['tags'] = $tags;
     echo json_encode($user);
 } else {
     $logparams = array('other' => array('cause' => 'invalid_token'));
